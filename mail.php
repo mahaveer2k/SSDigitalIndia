ca66957746d311c128e29158c7bf9654b5ee5ca0
Akshay Rana Gujjar <akshaybhati28@gmail.com>
2019-07-17 23:54:55 +0530

    add: mailer

--- mail.php
+++ mail.php
@@ -1,8 +1,17 @@
 <?php
 
+// https://github.com/PHPMailer/PHPMailer/wiki/Using-Gmail-with-XOAUTH2 <= SMTP ERROR: Password command failed:
+
 error_reporting(-1);
 ini_set('display_errors', 'On');
 set_error_handler("var_dump");
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'vendor/phpmailer/phpmailer/src/Exception.php';
+require 'vendor/phpmailer/phpmailer/src/PHPMailer.php';
+require 'vendor/phpmailer/phpmailer/src/SMTP.php';
 
 $mail = new PHPMailer(); // create a new object
 $mail->IsSMTP(); // enable SMTP
@@ -12,12 +21,12 @@ $mail->SMTPSecure = 'ssl'; // secure transfer enabled REQUIRED for Gmail
 $mail->Host = "smtp.gmail.com";
 $mail->Port = 465; // or 587
 $mail->IsHTML(true);
-$mail->Username = "email@gmail.com";
-$mail->Password = "password";
-$mail->SetFrom("example@gmail.com");
+$mail->Username = "satyagangacompetitionclasses@gmail.com";
+$mail->Password = "satyaganga@12345";
+$mail->SetFrom("satyagangacompetitionclasses@gmail.com");
 $mail->Subject = "Test";
 $mail->Body = "hello";
-$mail->AddAddress("email@gmail.com");
+$mail->AddAddress("satyagangacompetitionclasses@gmail.com");
 
  if(!$mail->Send()) {
     echo "Mailer Error: " . $mail->ErrorInfo;
