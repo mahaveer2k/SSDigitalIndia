12eb1a1411934900f55e87ecb2dd5958657642bd
akshay-rana-gujjar <akshaybhati28@gmail.com>
2019-07-30 14:59:51 +0530

    add: tawk.to

--- index.php
+++ index.php
@@ -480,6 +480,21 @@ function getCallbackUrl()
     }
     //--
     </script>
+
+    <!--Start of Tawk.to Script-->
+    <script type="text/javascript">
+    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+    (function(){
+    var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+    s1.async=true;
+    s1.src='https://embed.tawk.to/5d4009ce7d27204601c85a97/default';
+    s1.charset='UTF-8';
+    s1.setAttribute('crossorigin','*');
+    s0.parentNode.insertBefore(s1,s0);
+    })();
+    </script>
+    <!--End of Tawk.to Script-->
+
 </body>
 
 </html>
