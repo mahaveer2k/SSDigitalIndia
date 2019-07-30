33b00a21703117a363b9598268fce078ac0c6389
akshay-rana-gujjar <akshaybhati28@gmail.com>
2019-07-30 18:14:23 +0530

    fix error

--- selectTest.php
+++ selectTest.php
@@ -6,9 +6,22 @@ set_error_handler("var_dump");
 
 require("./connection.php");
 
+$s = "size";
+$q = "qunatity";
+$a = "this->amount";
+$o = "this->orderID";
+$f = "this->first_name";
+$m = "this->mobile";
+$e = "this->email";
+$add = "this->address";
+$ci = "this->city";
+$p = "this->pin_code";
+$country = "this->country";
+$i_path = "this->image_path";
+
 
 $stmt = $conn->prepare('INSERT INTO orders (size, quantity, amount, order_id, first_name, mobile, email, address, city, pin_code, country, image_path) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)');
-$stmt->bind_param("ssssssssssss","20.00", "5", "100", "xyz", "xas", "xyz", "xas", "xyz", "xas", "xas", "xas", "xas");
+$stmt->bind_param("ssssssssssss", $s, $q, $a, $o, $f, $m, $e, $add, $ci, $p, $country, $i_path);
 $stmt->execute();
 
 
